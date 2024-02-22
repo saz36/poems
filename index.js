9ebec96b50d857b04842a26a9ad17a65697ab644
saz36 <124158555+saz36@users.noreply.github.com>
2024-02-22 10:40:58 +0000

    javascript and type writer
    basic java

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-prompt("hello world");
+function generatePoem(event){
+event.preventDefault();
+new Typewriter('#poem', {
+    strings: "ha ha",
+    autoStart: true,
+    delay:1,
+    cursor:"",
+  });
+}
+let poemElememtForm =document.querySelector("#poem-generator");
+poemElememtForm.addEventListener("submit", generatePoem);
