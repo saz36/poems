81ecc4fd0f76cf860abcd10c83ec0b5410a417a2
saz36 <124158555+saz36@users.noreply.github.com>
2024-02-23 13:25:22 +0000

    added javascript api and axios

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+function displayPoem(response){
+    console.log("poem start");
+    new Typewriter('#poem', {
+        strings: response.data.answer,
+        autoStart: true,
+        delay:1,
+        cursor:"",
+      });
+    }
+
 function generatePoem(event){
 event.preventDefault();
-new Typewriter('#poem', {
-    strings: "ha ha",
-    autoStart: true,
-    delay:1,
-    cursor:"",
-  });
+
+let inputInstructions= document.querySelector("#input-user");
+let apiKey = `ccedafe30c0o08ea49bb7fb493t06576`;
+let prompt =`Generate a poem about ${inputInstructions.value}`;
+let context = "be kind and polite give maximum 10 lines and seperate with <br />";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+console.log("poem start");
+console.log(`prompt: ${prompt}`)
+console.log(`prompt: ${context}`)
+axios.get(apiUrl).then(displayPoem);
 }
+
 let poemElememtForm =document.querySelector("#poem-generator");
 poemElememtForm.addEventListener("submit", generatePoem);
